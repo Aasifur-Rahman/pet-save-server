39ee24d0435de0e563819e40edff32734984a82c
Siam Rahman <asifur.web@gmail.com>
2024-08-29 20:31:39 +0600

    added single data access by id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8zp6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,13 @@ async function run() {
     app.get("/pets", async (req, res) => {
       const cursor = petsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/pets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petsCollection.findOne(query);
       res.send(result);
     });
 
