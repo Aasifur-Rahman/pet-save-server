6a04115df2be3e7aaed67e426a453dcf7a8810e5
Siam Rahman <asifur.web@gmail.com>
2025-01-11 03:39:13 +0600

    added jwt middle ware verify token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +29,30 @@ async function run() {
     const fosterCollection = client.db("petsaveDB").collection("fosterPet");
     const userCollection = client.db("petsaveDB").collection("users");
 
+    // jwt related API's
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     //pets related API's
     app.get("/pets", async (req, res) => {
       const result = await petsCollection.find().toArray();
@@ -43,7 +68,7 @@ async function run() {
 
     // user related API's
 
-    app.post("/users", async (req, res) => {
+    app.post("/user", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
