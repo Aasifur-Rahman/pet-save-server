1980234ae25e5f007850c53c08025a227d97a956
Siam Rahman <asifur.web@gmail.com>
2025-01-19 13:07:58 +0600

    all issues resolved regarding profile

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ async function run() {
       res.send({ userDetails: result });
     });
 
+    app.get("/users/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      console.log("from id", result);
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
